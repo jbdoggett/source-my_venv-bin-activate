826d569114c3d6e7bfdd814b0adc5b1a38c8328f
Jesse Doggett <doggett@stsci.edu>
2024-05-15 17:07:24 -0400

    Now added Albums

--- first_api/music/views.py
+++ first_api/music/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 
-from music.models import Artist
-from music.serializers import ArtistSerializer
+from music.models import Artist, Album
+from music.serializers import ArtistSerializer, AlbumSerializer
 
 # Create your views here.
 
@@ -14,3 +16,59 @@ def index(_request):
 class ArtistViewSet(viewsets.ModelViewSet):
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
+
+class AlbumViewSet(viewsets.ViewSet):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+    def list(self, request):
+        """
+        List the resources, albums in this case
+        """
+        serializer = self.serializer_class(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        """
+        List the resources, albums in this case
+        """
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):
+        """
+        Retrieve a single resources, album in this case
+        """
+        album = get_object_or_404(Album, pk=pk)
+        serializer = self.serializer_class(album)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        """
+        Update the resource, album in this case
+        """
+        album = get_object_or_404(Album, pk=pk)
+        serializer = self.serializer_class(album, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def partial_update(self, request, pk=None):
+        """
+        Partially update the resource, album in this case
+        """
+        album = get_object_or_404(Album, pk=pk)
+        serializer = self.serializer_class(album, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        """
+        Delete the resource, album in this case
+        """
+        album = get_object_or_404(Album, pk=pk)
+        album.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
